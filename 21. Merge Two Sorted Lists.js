7a657da718fcc1c89c24edbab4589ad097c16c3c
Tuğcan Kartal <tugcankartal@ankuni.intra>
2025-06-26 11:32:16 +0300

    21.algorithm solution added

---
+++ 21. Merge Two Sorted Lists.js
@@ -0,0 +1,24 @@
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+}
+
+var mergeTwoLists = function(list1, list2) {
+    let dummy=new ListNode(-1);
+    let current=dummy;
+
+    while(list1!==null && list2!==null){
+        if(list1.val<list2.val){
+            current.next=list1;
+            list1=list1.next;
+        }else{
+            current.next=list2;
+            list2=list2.next;
+        }
+
+        current=current.next;
+    }
+
+    current.next=list1!==null ? list1 : list2; 
+    return dummy.next;
+};
