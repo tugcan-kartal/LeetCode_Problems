56bfe198dc9d1543ac207f978157178e0656a529
tugcankartal <tugcankartal@BID-TUGCANKAR>
2025-08-04 10:57:01 +0300

    2721.javascript solution added

---
+++ leetcode/30daysof-javascript/2721. Execute Asynchronous Functions in Parallel.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+    const promises=functions.map((fn)=>fn());
+
+    return Promise.all(promises);
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
