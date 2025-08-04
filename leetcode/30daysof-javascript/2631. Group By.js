6337351642ee8d1bc90aaada726f0c41c9038dcb
tugcankartal <tugcankartal@BID-TUGCANKAR>
2025-08-04 14:15:07 +0300

    2631.javascript solution added

---
+++ leetcode/30daysof-javascript/2631. Group By.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    const result={};
+
+    for(let item of this){
+        let key=fn(item);
+
+        if(!result[key]){
+            result[key]=[];
+        }
+
+        result[key].push(item);
+    }
+
+    return result;
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
